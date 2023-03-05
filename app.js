9dc07f6ccfc41bc367a1cbbc153afdbcc3646621
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-05 20:13:23 +0530

    updated

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+require('./db/connect');
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
 
 const port = 3000;
 
+//Middleware
+
+app.use(express.json());
+
 //routes
 app.get('/', (req, res) => {
-    res.send('Task Manager App')
-})
+    res.send('Task Manager App');
+});
 
-app.listen(port, console.log(`App is listening on port ${port}`)
-)
+app.use('/api/v1/tasks', tasks);
+
+app.listen(port, console.log(`App is listening on port ${port}`));
