259c9e545d6baa90970408754d61eac78c5ca830
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-06 08:33:34 +0530

    added find functionallity

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,30 @@
-const getAllTasks = (req, res) => {
-    res.send('Get All Task');
+const task = require('../modals/task');
+const Task = require('../modals/task');
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 };
 
-const createTask = (req, res) => {
-    res.json(req.body);
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({ task });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 };
 
-const getTask = (req, res) => {
-    res.json({id: req.params.id});
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOne({ _id: taskID });
+        res.status(200).json({ task });
+    } catch (error) {}
 };
 
 const updateTask = (req, res) => {
