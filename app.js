798fc8187052dc8a7979caaac2a3f57aeb132e99
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-07 06:42:49 +0530

    added middleware

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB =  require('./db/connect');
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
 
 const port = 3000;
 
 //Middleware
 
+app.use(express.static('./public'))
 app.use(express.json());
 
 //routes
-app.get('/', (req, res) => {
-    res.send('Task Manager App');
-});
 
 app.use('/api/v1/tasks', tasks);
+
+app.use(notFound)
 
 const start = async () => {
     try {
